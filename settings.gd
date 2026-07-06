97b4ad904edce9d2516e65ade485e7086d490405
DEVUCP <DEVUCP@protonmail.com>
2026-07-06 17:19:21 +0300

    fix: settings with main menu parity

---
+++ settings.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var mouse_sens: float = 0.35
+var toggle_sprint_setting: bool = false
+
+func set_mouse_sens(val: float)-> void:
+	mouse_sens = val
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
